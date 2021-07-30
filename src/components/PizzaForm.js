3d814681c5e2225891c684c200570cfa1d7d6ca7
ccchippp <clempke90@gmail.com>
2021-07-30 08:49:42 -0400

    pizza form

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,17 +1,35 @@
 import React from "react"
 
 class PizzaForm extends React.Component {
+  state = {
+    formPizza: this.props.selectedPizza
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.name, e.target.value)
+    this.setState( {
+      formPizza: {
+        ...this.state.formPizza,
+        [e.target.name]: e.target.value 
+      }
+    })
+  }
+
   render() {
     return(
         <div className="form-row">
           <div className="col-5">
-              <input type="text" className="form-control" placeholder="Pizza Topping" value={
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Pizza Topping" 
+                value={this.state.formPizza.topping}
+                onChange={this.handleChange}
                   //Pizza Topping Should Go Here
-                  null
-                }/>
+                />
           </div>
           <div className="col">
-            <select value={null} className="form-control">
+            <select value={this.state} className="form-control">
               <option value="Small">Small</option>
               <option value="Medium">Medium</option>
               <option value="Large">Large</option>
